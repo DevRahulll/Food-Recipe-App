2e303c4f6911709fb1723c75261be068488a412d
Rahul <rahuldevan2019@gmail.com>
2023-10-15 23:32:53 +0530

    Intializing all files

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchInput=document.getElementById('search-box')
+const searchButton=document.getElementById('searchButton')
+const foodList=document.getElementById('foodList')
+const Conatainer=document.querySelector(".conataine");
+const recipeCloseBtn=document.querySelector("closeButton")
+const foodDetailsContent=document.querySelector("food-details-content")
