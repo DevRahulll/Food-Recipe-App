0dddc6141d52fdaec8077358c2fa8570ea39f8ea
Rahul <rahuldevan2019@gmail.com>
2023-10-17 00:48:01 +0530

    completed whole project

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const searchInput=document.getElementById('search-box')
 const searchButton=document.getElementById('searchButton')
 const mealList=document.getElementById('mealList')
-const Conatainer=document.querySelector(".conataine");
+const Conatainer=document.querySelector(".container");
 const recipeCloseBtn=document.getElementById("recipeCloseBtn")
-const foodDetailsContent=document.querySelector(".meal-details-content")
+const mealDetailsContent=document.querySelector(".meal-details-content")
 
 
 // Event Listener
@@ -43,7 +43,7 @@ async function getMealDetails(mealId){
     try {
         const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
         const data=await response.json();
-        return data.meal[0];
+        return data.meals[0];
     } catch (error) {
         console.error("Error in fetching meals Details:",error);
     }
@@ -64,3 +64,46 @@ function displayMeals(meal){
         mealList.innerHTML='<p>No meals found. Try another Recipe.</p>';
     }
 }
+// Function to create and display meal details on popup
+function showMealDetailsPopup(meal){
+    mealDetailsContent.innerHTML=`
+    <h2 class='recipe-title'>${meal.strMeal}</h2>
+    <p class="recipe-category">${meal.strCategory}</p>
+    <div class="recipe-instruct">
+    <h3>Instructions:</h3>
+    <p>${meal.strInstructions}</p>
+    </div>
+    <div class="recipe-img">
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+    </div>
+    <div class="recipe-video">
+    <a href="${meal.strYoutube}" target="_blank">Video Tutorial</a>
+    </div>
+    `;
+    Conatainer.style.display='block';
+}
+
+// Event listener for popup close button
+recipeCloseBtn.addEventListener('click',closeRecipe);
+function closeRecipe(){
+    Conatainer.style.display='none';
+}
+
+searchInput.addEventListener('keyup',(e)=>{
+    if(e.key==='Enter'){
+        performSearch();
+    }
+});
+
+async function performSearch(){
+    const ingredient=searchInput.value.trim();
+    if(ingredient){
+        const meals=await searchMealsByIngredient(ingredient);
+        displayMeals(meals);
+    }
+}
+// Perform a chicken search on page load
+window.addEventListener('load',()=>{
+    searchInput.value='chicken';
+    performSearch();
+});
